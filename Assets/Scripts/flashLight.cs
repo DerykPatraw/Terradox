8e190bb48fc1ee2b18e54600a6049ac3cbcc7294
beldm175 <noodl3dud3@gmail.com>
2015-12-09 15:13:34 -0800

    Added FlashLight Script and level one terrain

---
+++ Assets/Scripts/flashLight.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flashLight : MonoBehaviour {
+
+	public Light myLight;
+
+	void Update () {
+		if (Input.GetKeyDown("f")){
+			myLight.enabled = !myLight.enabled;
+		}
+	}
+}
+
