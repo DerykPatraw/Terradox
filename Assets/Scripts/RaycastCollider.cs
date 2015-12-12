2d66532c1f5114fbf13ad05ee3c0edab607e0734
DerykPatraw <Dpatraw219@gmail.com>
2015-12-12 13:52:15 -0800

    Added Raycasting

---
+++ Assets/Scripts/RaycastCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastCollider : MonoBehaviour {
+
+	void Update() {
+		Vector3 fwd = transform.TransformDirection(Vector3.forward); 
+		if (Physics.Raycast (transform.position, fwd, 3)) { //shoot ray forward. If ray hits something...
+			print ("There is something in front of the object!");
+		}
+		else {
+			print("There is nothing here.");
+		}
+	}
+}
