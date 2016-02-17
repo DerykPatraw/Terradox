173b15db0cc6f5089c5ba5ed1794bd4147c08121
DerykPatraw <Dpatraw219@gmail.com>
2016-02-17 14:40:00 -0800

    Added Present 3 marble

--- Assets/Scripts/RaycastCollider.cs
+++ Assets/Scripts/RaycastCollider.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
 
 public class RaycastCollider : MonoBehaviour {
 
@@ -13,32 +12,29 @@ public class RaycastCollider : MonoBehaviour {
 		ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f)); //ray is where camera is pointing
 		if (Physics.Raycast (ray, out hit, range)) { //shoot ray forward a distance. If ray hits something...
 
-			if (hit.collider.tag == "Marble" && Input.GetKeyDown("e")) { //sees if the button is pressed and if it's a marble...
-				Application.LoadLevel(1);
-			}
+			if (hit.collider.tag == "Marble" && Input.GetKeyDown ("e")) { //sees if the button is pressed and if it's a marble...
+				Application.LoadLevel (1);
 
-			else if (hit.collider.tag == "MarblePresent1" && Input.GetKeyDown("e")){
-				Application.LoadLevel(2);
-			}
+			} else if (hit.collider.tag == "MarblePresent1" && Input.GetKeyDown ("e")) {
+				Application.LoadLevel (2);
 
-			else if (hit.collider.tag == "MarbleFarPast" && Input.GetKeyDown("e")){
-				Application.LoadLevel(3);
-			}
+			} else if (hit.collider.tag == "MarbleFarPast" && Input.GetKeyDown ("e")) {
+				Application.LoadLevel (3);
 
-			else if (hit.collider.tag == "MarblePast" && Input.GetKeyDown("e")){
-				Application.LoadLevel(4);
-			}
+			} else if (hit.collider.tag == "MarblePast" && Input.GetKeyDown ("e")) {
+				Application.LoadLevel (4);
 
-			else if (hit.collider.tag == "MarblePresent2" && Input.GetKeyDown("e")){
-				Application.LoadLevel(5);
-			}
+			} else if (hit.collider.tag == "MarblePresent2" && Input.GetKeyDown ("e")) {
+				Application.LoadLevel (5);
 
-			else if (hit.collider.tag == "MarbleFuture" && Input.GetKeyDown("e")){
-				Application.LoadLevel(6);
-			}
+			} else if (hit.collider.tag == "MarbleFuture" && Input.GetKeyDown ("e")) {
+				Application.LoadLevel (6);
 
-			else if (hit.collider.tag == "MarbleFarFuture" && Input.GetKeyDown("e")){
-				Application.LoadLevel(7);
+			} else if (hit.collider.tag == "MarbleFarFuture" && Input.GetKeyDown ("e")) {
+				Application.LoadLevel (7);
+
+			} else if (hit.collider.tag == "MarblePresent3" && Input.GetKeyDown ("e")) {
+				Application.LoadLevel (0);
 			}
 
 			else if (hit.collider.tag == "MarbleExit" && Input.GetKeyDown("e")){
