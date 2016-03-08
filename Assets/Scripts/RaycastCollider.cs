2e435f067b1de6b576f510df0c879f54f27c75c2
beldm175 <noodl3dud3@gmail.com>
2016-03-08 15:29:34 -0800

    AIand stuff

--- Assets/Scripts/RaycastCollider.cs
+++ Assets/Scripts/RaycastCollider.cs
@@ -15,28 +15,37 @@ public class RaycastCollider : MonoBehaviour {
 			if (hit.collider.tag == "Marble" && Input.GetKeyDown ("e")) { //sees if the button is pressed and if it's a marble...
 				Application.LoadLevel (1);
 
-			} else if (hit.collider.tag == "MarblePresent1" && Input.GetKeyDown ("e")) {
+			} 
+			else if (hit.collider.tag == "MarblePresent1" && Input.GetKeyDown ("e")) {
 				Application.LoadLevel (2);
 
-			} else if (hit.collider.tag == "MarbleFarPast" && Input.GetKeyDown ("e")) {
+			} 
+			else if (hit.collider.tag == "MarbleFarPast" && Input.GetKeyDown ("e")) {
 				Application.LoadLevel (3);
 
-			} else if (hit.collider.tag == "MarblePast" && Input.GetKeyDown ("e")) {
+			} 
+			else if (hit.collider.tag == "MarblePast" && Input.GetKeyDown ("e")) {
 				Application.LoadLevel (4);
 
-			} else if (hit.collider.tag == "MarblePresent2" && Input.GetKeyDown ("e")) {
+			} 
+			else if (hit.collider.tag == "MarblePresent2" && Input.GetKeyDown ("e")) {
 				Application.LoadLevel (5);
 
-			} else if (hit.collider.tag == "MarbleFuture" && Input.GetKeyDown ("e")) {
+			} 
+			else if (hit.collider.tag == "MarbleFuture" && Input.GetKeyDown ("e")) {
 				Application.LoadLevel (6);
 
-			} else if (hit.collider.tag == "MarbleFarFuture" && Input.GetKeyDown ("e")) {
+			} 
+			else if (hit.collider.tag == "MarbleFarFuture" && Input.GetKeyDown ("e")) {
 				Application.LoadLevel (7);
 
-			} else if (hit.collider.tag == "MarblePresent3" && Input.GetKeyDown ("e")) {
+			} 
+			else if (hit.collider.tag == "MarblePresent3" && Input.GetKeyDown ("e")) {
 				Application.LoadLevel (0);
 			}
-
+			else if (hit.collider.tag == "MarbleTutorial" && Input.GetKeyDown ("e")) {
+				Application.LoadLevel (8);
+			}
 			else if (hit.collider.tag == "MarbleExit" && Input.GetKeyDown("e")){
 				Application.Quit();
 			}
