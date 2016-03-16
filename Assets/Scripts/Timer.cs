eeebb96c594d857847638049e30a44696535ce53
beldm175 <noodl3dud3@gmail.com>
2016-03-16 14:49:53 -0700

    added real timer animation

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,25 +3,22 @@ using UnityEngine.UI;
 using System.Collections;
 public class Timer : MonoBehaviour {
 
-	float timeRemaining; //number that counts down
-	string timeLeft; //what user sees
+	float timeRemaining = 0.0f; 
 
-	public Text text;
+	public Text timer; 
 	
 	// Update is called once per frame
 	void Update() {
 		timeRemaining -= Time.deltaTime; //count down timeRemaining upon start
-		timeLeft = string.Format("{0:0.0}", timeRemaining); //Show user the time 
-		print(timeLeft); //show how much time is remaining
 
-		text.text = timeLeft; //display time on screen
+		timer.text = timeRemaining.ToString ("0"); //display timer second-per-second
 				
 		if (timeRemaining < 0) {
 			Application.LoadLevel(0); //when timer reaches zero, load spash plaige
 		}
 
 		if (Application.loadedLevel == 0) {
-			timeRemaining = 650.0f; //set initial time
+			timeRemaining = 300.0f; //set initial time
 		}
 	}
 }
