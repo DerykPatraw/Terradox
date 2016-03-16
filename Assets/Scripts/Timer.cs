1a1c32c9df5f18fbcbdcc54908fb3e951fe51ef5
DerykPatraw <Dpatraw219@gmail.com>
2016-03-16 15:25:07 -0700

    hopefully fixed timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 public class Timer : MonoBehaviour {
 
-	float timeRemaining = 0.0f; 
+	static float timeRemaining = 300.0f; 
 
 	public Text timer; 
 	
