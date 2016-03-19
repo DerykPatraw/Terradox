493dae7d47afd818184d84aad3396a785ee3c90a
Deryk Patraw <Deryk Patraw>
2016-03-19 14:02:16 -0700

    added timer script to master

--- Assets/Scripts/aiFollow.cs
+++ Assets/Scripts/aiFollow.cs
@@ -5,18 +5,25 @@ public class aiFollow : MonoBehaviour {
 
 	Transform tr_Player;
 	float f_RotSpeed=3.0f,f_MoveSpeed = 3.0f;
-	
+
 	// Use this for initialization
 	void Start () {
-		
+
 		tr_Player = GameObject.FindGameObjectWithTag ("Player").transform; }
-	
+
 	// Update is called once per frame
 	void Update () {
 		/* Look at Player*/
 		transform.rotation = Quaternion.Slerp (transform.rotation , Quaternion.LookRotation (tr_Player.position - transform.position) , f_RotSpeed * Time.deltaTime);
-		
+
 		/* Move at Player*/
 		transform.position += transform.forward * f_MoveSpeed * Time.deltaTime;
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.GetComponent<Collider>().tag == "Player")
+		{
+			Application.LoadLevel (0);
+		}
 	}
 }
