59110c3957fc5ed5f7a67679e57772a0170e1008
Deryk Patraw <Deryk Patraw>
2016-03-20 12:54:22 -0700

    Added marble colliders and deleted raycasting

---
+++ Assets/Scripts/SphereCollider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereCollider : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<Collider> ().tag == "Player" && this.gameObject.tag == "Marble") {
+			Application.LoadLevel (Application.loadedLevel + 1);
+		}
+		else if (other.GetComponent<Collider> ().tag == "Player" && this.gameObject.tag == "MarbleExit") {
+			Application.Quit ();
+		}
+		else if (other.GetComponent<Collider> ().tag == "Player" && this.gameObject.tag == "MarblePresent3") {
+			Application.LoadLevel (0);
+		}
+		else if (other.GetComponent<Collider> ().tag == "Player" && this.gameObject.tag == "MarbleTutorial") {
+			Application.LoadLevel (0);
+		}
+	}
+}
