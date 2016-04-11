467a2068aba21f42262c743277fab20b53a5691a
Micah Belden <Micah Belden>
2016-04-11 08:56:07 -0700

    some new sounds and attempted marble sound script

---
+++ Assets/Scripts/audioMarbles.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class audioMarbles : MonoBehaviour {
+		void OnTriggerEnter () {
+		AudioSource audio = GetComponent<AudioSource>();
+			audio.Play();
+		}
+}
