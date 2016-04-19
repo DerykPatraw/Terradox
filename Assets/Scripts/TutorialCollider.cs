057e2d8ad377f962fef89c27e5470993ad320094
Deryk Patraw <Deryk Patraw>
2016-04-18 21:15:17 -0700

    Bug fixes, final version

--- Assets/Scripts/TutorialCollider.cs
+++ Assets/Scripts/TutorialCollider.cs
@@ -5,9 +5,11 @@ public class TutorialCollider : MonoBehaviour {
 
 	public GameObject Player1;
 	public GameObject Player2;
+	public GameObject SpawnTimer;
 
 	void OnTriggerEnter(Collider other) {
 		if (other.GetComponent<Collider> ().tag == "Player" && this.gameObject.tag == "TutorialExit") {
+			Destroy (SpawnTimer);
 			Destroy (Player2);
 			Application.LoadLevel (1);
 		}
